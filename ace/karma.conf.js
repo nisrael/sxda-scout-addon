492cca08a16061104964bbb52fc9f166b2e2023f
Nils Israel <nils@sxda.io>
2024-01-21 00:46:45 +0100

    Fix license headers

--- ace/karma.conf.js
+++ ace/karma.conf.js
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2010-2023 BSI Business Systems Integration AG
- * Copyright (c) 2023 Nils Israel
+ *  Copyright (c) 2010-2024 BSI Business Systems Integration AG
+ *  Copyright (c) 2023-2024 Nils Israel
  *
- * This program is based on work from the Eclipse Scout Project
- * https://www.eclipse.org/scout/ and provides an extension for it.
+ * This program is an extension of the original work from the Eclipse Scout Project,
+ * available at https://www.eclipse.org/scout/.
  *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *  This program and the accompanying materials are made
+ *  available under the terms of the Eclipse Public License 2.0
+ *  which is available at https://www.eclipse.org/legal/epl-2.0/
  *
- * SPDX-License-Identifier: EPL-2.0
+ *  SPDX-License-Identifier: EPL-2.0
  */
 const baseConfig = require('@eclipse-scout/cli/scripts/karma-defaults');
 
--- ace/src/ace/AceField.ts
+++ ace/src/ace/AceField.ts
@@ -1,17 +1,16 @@
 /*
- * Copyright (c) 2023 BSI Business Systems Integration AG
- * Copyright (c) 2023 Nils Israel
+ *  Copyright (c) 2010-2024 BSI Business Systems Integration AG
+ *  Copyright (c) 2023-2024 Nils Israel
  *
- * This program is based on work from the Eclipse Scout Project
- * https://www.eclipse.org/scout/ and provides an extension for it.
+ * This program is an extension of the original work from the Eclipse Scout Project,
+ * available at https://www.eclipse.org/scout/.
  *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *  This program and the accompanying materials are made
+ *  available under the terms of the Eclipse Public License 2.0
+ *  which is available at https://www.eclipse.org/legal/epl-2.0/
  *
- * SPDX-License-Identifier: EPL-2.0
+ *  SPDX-License-Identifier: EPL-2.0
  */
-
 import {InitModelOf, ValueField} from '@eclipse-scout/core';
 import {AceFieldModel} from "./AceFieldModel";
 import {AceFieldEventMap} from "./AceFieldEventMap";
