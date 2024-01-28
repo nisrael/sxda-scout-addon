336774e50d40c0b00e4e8a561bf2821bb3a1fc2e
Nils Israel <nils@sxda.io>
2024-01-28 10:50:30 +0100

    update license headers

--- demo/src/desktop/DesktopModel.ts
+++ demo/src/desktop/DesktopModel.ts
@@ -1,3 +1,16 @@
+/*
+ *  Copyright (c) 2010-2024 BSI Business Systems Integration AG
+ *  Copyright (c) 2023-2024 Nils Israel
+ *
+ * This program is an extension of the original work from the Eclipse Scout Project,
+ * available at https://www.eclipse.org/scout/.
+ *
+ *  This program and the accompanying materials are made
+ *  available under the terms of the Eclipse Public License 2.0
+ *  which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ */
 import {AceForm} from "../ace/AceForm";
 import {Desktop} from "./Desktop";
 import {DemoOutline} from "../index";
