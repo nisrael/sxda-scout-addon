85d7ace49f6f0abee6edf36e17333bd6268c7db3
Nils Israel <nils@sxda.io>
2024-05-06 22:33:28 +0200

    reformat license headers

--- ace/src/ace/AceFieldEnterKeyStroke.ts
+++ ace/src/ace/AceFieldEnterKeyStroke.ts
@@ -1,5 +1,9 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010-2024 BSI Business Systems Integration AG
+ * Copyright (c) 2023-2024 Nils Israel
+ *
+ * This program is an extension of the original work from the Eclipse Scout Project,
+ * available at https://www.eclipse.org/scout/.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
